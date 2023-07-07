fed60e273bf6590ff686f2f15159b38869df0dfd
Lizzz <alejandroraidi@gmail.com>
2023-07-07 15:49:53 -0600

    start of routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -10,4 +10,32 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/dashboard', async (req, res) => {
+    try {
+        res.render("dashboard");
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Internal server error"});
+    }
+});
+
+router.get('/signup', async (req, res) => {
+    try {
+        res.render("signup");
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Internal server error"});
+    }
+});
+
+
+router.get('/login', async (req, res) => {
+    try {
+        res.render("login");
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Internal server error"});
+    }
+});
+
 module.exports = router;
