b5cda49921b92712259438fb33ff2d28930ea735
Lizzz <alejandroraidi@gmail.com>
2023-07-12 16:28:01 -0600

    retrieve posts in homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,14 @@
 const router = require('express').Router();
+const Post = require('../models/Post');
 
 
 router.get('/', async (req, res) => {
     try {
-        res.render("homepage");
+        const postData = await Post.findAll().catch((err) =>{
+            res.json(err);
+        })
+        const posts = postData.map((post) =>post.get({ plain:true }));
+        res.render("homepage", { posts });
     } catch (err) {
         console.log(err);
         res.status(500).json({ message: "Internal server error"});
