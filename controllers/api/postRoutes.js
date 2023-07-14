d12fab4499df5f4774bcaa1aaa0b28f75da7cce9
Lizzz <alejandroraidi@gmail.com>
2023-07-14 15:24:12 -0600

    removed a comment

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const {User, Post, Comment} = require('../../models'); 
 
-//UNA RUTA CON EL ID DE UN POST PARA DESPLEGAR ESE POST Y TODOS SUS COMMENTS
-//RENDER THE POST PAGE WHERE THE POST ID MATCHES THE QUERY PARAMS
 router.get('/:id', async(req, res) => {
     try {
         const postData = await Post.findByPk(req.params.id,{
